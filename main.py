be062b1ba740b4271154d486de9bbafcbae99833
Dheeruvaii <dhirajsapkota260@gmail.com>
2024-02-13 10:54:34 +0545

    hold screen time using while loop in a event

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pygame
 pygame.init()
-dis=pygame.display.set_mode((400,400))
+dis=pygame.display.set_mode((400,400)) 
+"""create screen using pygame library and display.set_mode()"""
 pygame.display.update()
+pygame.display.set_caption('Dheeruvaii_Snake_Game')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            game_over=True
+
 pygame.quit()
 quit()
